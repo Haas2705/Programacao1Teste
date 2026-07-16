9ab936383cc7648509d764f968d0e5ee43bb0fe4
Gustavo <gugahaas2002@gmail.com>
2026-07-16 19:31:25 -0300

    Resolução dos exercicios 7,8 e 9

--- Semanas11e12/Exercicios.java
+++ Semanas11e12/Exercicios.java
@@ -9,9 +9,44 @@ public class Exercicios {
 
         Exercicios testes = new Exercicios();
 
+        // Teste exercicio 01
         testes.exercicio01();
+
+        // Teste exercicio 02
         testes.exercicio02();
-        testes.exercicio03();
+
+        // Teste exercicio 03
+        // testes.exercicio03();
+
+        // Teste exercicio 04
+        int[] arrayEx04 = new int[] { 1, 2, 3, 4, 5, 6, -5, -10 };
+        int resultado = testes.exercicio04(arrayEx04);
+        System.out.println("A quantidade de números negativos é: " + resultado);
+
+        // Teste exercicio 05
+        int[] arrayEx05 = new int[] { 1, 2, 3, 4, 5, 6, 6, 6, 7, 8, 9 };
+        int ocorrencias = testes.exercicio05(arrayEx05, 6);
+        System.out.println("O número de ocorrencias de X é: " + ocorrencias);
+
+        // Teste exercicio 06
+        int[] arrayEx06 = new int[] { 1, 2, -4, -9 };
+        boolean[] resultado2 = testes.exercicio06(arrayEx06);
+        System.out.println(Arrays.toString(resultado2));
+
+        // Teste exercicio 07
+        double[] arrayEx07 = new double[] { 1.5, 1.2, 2.4, 5 };
+        int resultado07 = testes.exercicio07(arrayEx07);
+        System.out.println(resultado07);
+
+        // Teste exercicio 08
+        // int[] resultado08 = testes.exercicio08();
+        // System.out.println(Arrays.toString(resultado08));
+
+        // Teste exercicio 09
+        int[] arrayEx09 = new int[] { 1, 2, 4, 6, 9, 5 };
+        System.out.println("Antes: " + Arrays.toString(arrayEx09));
+        testes.exercicio09(arrayEx09);
+        System.out.println("Depois: " + Arrays.toString(arrayEx09));
 
     }
 
@@ -20,7 +55,7 @@ public class Exercicios {
         int[] array1 = new int[10];
 
         for (int i = 0; i < array1.length; i++) {
-            array1[i] = 10 - i;
+            array1[i] = array1.length - i;
         }
         System.out.println(Arrays.toString(array1));
 
@@ -78,8 +113,88 @@ public class Exercicios {
         System.out.println(Arrays.toString(arrayd));
     }
 
-    public void exercicio04() {
+    public int exercicio04(int[] array) {
+        if (array == null)
+            return -1;
 
+        int quant = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) {
+                quant++;
+            }
+        }
+        return quant;
+    }
+
+    public int exercicio05(int[] a, int x) {
+        if (a == null)
+            return -1;
+
+        int ocorrencias = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == x) {
+                ocorrencias++;
+            }
+        }
+        return ocorrencias;
+    }
+
+    public boolean[] exercicio06(int[] array) {
+        if (array == null)
+            return null;
+
+        boolean[] resultado = new boolean[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+            resultado[i] = array[i] > 0;
+        }
+        return resultado;
+    }
+
+    public int exercicio07(double[] array) {
+        if (array == null || array.length == 0)
+            return -1;
+
+        int pos = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > array[pos]) {
+                pos = i;
+            }
+        }
+        return pos;
+    }
+
+    public int[] exercicio08() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Qual o tamanho que você quer que tenha o array de inteiros? ");
+        int tam = sc.nextInt();
+
+        int[] arrayBase = new int[tam];
+
+        for (int i = 0; i < arrayBase.length; i++) {
+            System.out.print("Digite o valor da " + (i + 1) + "ª posição: ");
+            arrayBase[i] = sc.nextInt();
+        }
+
+        sc.close();
+        return arrayBase;
+    }
+
+    public void exercicio09(int[] array) {
+        if (array == null) {
+            return;
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                array[i] = 1;
+            } else {
+                array[i] = -1;
+            }
+        }
     }
 
 }
